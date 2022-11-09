956d7926cb1bd66de543aff2948fd6f0e6aaac7c
hongnt1110 <20521357@gm.uit.edu.vn>
2022-11-09 15:48:59 +0700

    add coe

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./style.css"
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div id="page">
+      <div id="header">
+        <div id="container">
+          <div class="left logo">
+            <img src="https://motchill.com/motchill.png?v1.0.2" alt="" />
+            <img src="" alt="" />
+          </div>
+          <input type="text" name="search" id="txtSearch" placeholder='v.d: tên phim, tên diễn viên,...' />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25" />
+          </svg>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
