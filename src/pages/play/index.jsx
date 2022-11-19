a1da53d340e06e19d503ea6600d243c559c90c8f
phuhuong <20521382.gm.uit.edu.vn>
2022-11-19 09:53:17 +0700

    push code

--- src/pages/play/index.jsx
+++ src/pages/play/index.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Play = () => {
   return (
-    <div className="bg-slate-900">Play</div>
+    <div className="bg-red-600">Play</div>
   )
-}
 
+
+var Video =React.createClass({
+  render: function(){
+     return(
+      <video width={520} height={380} controls >
+           <source src="" type="video/mp4"> </source>
+           <source src="" type="video/mp4"> </source>
+      </video>
+     );
+  }
+});
+}
 export default Play
