5c8fbbfc3274c69ca05bb83b1574a595bd5aa6e4
hongnt1110 <20521357@gm.uit.edu.vn>
2022-11-19 08:54:42 +0700

    push detail screen

---
+++ src/assets/data/listHighlights.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        image: "/public/assets/56.jpg",
+        Name: 'Highlight | One Piece EP232',
+        time: '03:01'
+    },
+    {
+        image: "/public/assets/57.jpg",
+        Name: 'Highlight | One Piece EP201',
+        time: '02:21'
+    },
+    {
+        image: "/public/assets/58.jpg",
+        Name: 'Highlight | One Piece EP462',
+        time: '03:23'
+    },
+    {
+        image: "/public/assets/59.jpg",
+        Name: 'Highlight | One Piece EP290',
+        time: '02:46'
+    },
+];
+export default data;
